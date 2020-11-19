7253c2f10187588679fd8d2157054e4f5d9e8b76
Richard <riicchhaarrd@users.noreply.github.com>
2020-11-19 14:42:15 +0100

    simple stream parse and dynamic string

---
+++ heap_string.h
@@ -0,0 +1,148 @@
+#ifndef HEAP_STRING_H
+#define HEAP_STRING_H
+
+#include <malloc.h>
+#include <string.h>
+#include <stdarg.h>
+
+struct heap_string_header
+{
+	int capacity;
+	int size;
+	char buf[];
+};
+
+#define HEAP_STRING_HDR(s) (struct heap_string_header *)( ( s ) - sizeof(struct heap_string_header) )
+
+//#define heap_string char*
+
+typedef char* heap_string;
+
+void heap_string_appendn(heap_string *s, const char *str, size_t n);
+void heap_string_append(heap_string *s, const char *str);
+#ifndef HEAP_STRING_IMPL
+
+heap_string heap_string_alloc(int n);
+heap_string heap_string_new(const char* s);
+void heap_string_free(heap_string *s);
+size_t heap_string_capacity(heap_string *s);
+size_t heap_string_size(heap_string *s);
+void heap_string_push(heap_string *s, int i);
+void heap_string_appendf(heap_string *s, const char *fmt, ...);
+#else
+heap_string heap_string_alloc(int n)
+{
+	struct heap_string_header *d = (struct heap_string_header*)malloc(sizeof(struct heap_string_header) + n + 1);
+	d->capacity = n;
+	d->size = 0;
+	return (heap_string)&d->buf[0];
+}
+
+//not binary safe
+heap_string heap_string_new(const char* s)
+{
+	int strsz = strlen(s);
+	struct heap_string_header *d = (struct heap_string_header*)malloc(sizeof(struct heap_string_header) + strsz + 1);
+	d->capacity = strsz;
+	d->size = d->capacity;
+	memcpy(d->buf, s, strsz + 1);
+	return d->buf;
+}
+
+void heap_string_free(heap_string *s)
+{
+	if (!s)
+		return;
+	if (!*s)
+		return;
+	struct heap_string_header *hdr = HEAP_STRING_HDR(*s);
+	free(hdr);
+	*s = NULL;
+}
+
+size_t heap_string_capacity(heap_string *s)
+{
+	struct heap_string_header *hdr = HEAP_STRING_HDR(*s);
+	return hdr->capacity;
+}
+
+size_t heap_string_size(heap_string *s)
+{
+	if(!s || !*s) return 0;
+	struct heap_string_header *hdr = HEAP_STRING_HDR(*s);
+	return hdr->size;
+}
+
+void heap_string_push(heap_string *s, int i)
+{
+	struct heap_string_header *hdr = 0;
+	if(*s)
+		hdr = HEAP_STRING_HDR(*s);
+	int newsize = 4;
+	if (*s)
+		newsize = hdr->size;
+
+	if (!*s || newsize + 1 >= hdr->capacity)
+	{
+		heap_string n = heap_string_alloc(newsize * 2 + 1);
+
+		if (*s)
+		{
+			heap_string_appendn(&n, *s, hdr->size);
+		}
+		heap_string_push(&n, i);
+		heap_string_free(s);
+		*s = n;
+	}
+	else {
+		hdr->buf[hdr->size] = i & 0xff;
+		++hdr->size;
+		hdr->buf[hdr->size] = '\0';
+	}
+}
+
+static inline void heap_string_transfer(heap_string* a, heap_string *b)
+{
+	*a = *b;
+	*b = NULL;
+}
+
+//not binary safe, avoid using the formatted functions for binary data
+void heap_string_appendf(heap_string *s, const char *fmt, ...)
+{
+#define NBUF (10)
+	static char buffer[NBUF][1024] = { 0 };
+	static int bufIndex = 0;
+
+	char *buf = &buffer[bufIndex++ % NBUF][0];
+
+	va_list args;
+	va_start(args, fmt);
+	vsnprintf(buf, sizeof(buffer[0]), fmt, args);
+	heap_string_append(s, buf);
+	va_end(args);
+}
+
+void heap_string_appendn(heap_string *s, const char *str, size_t n)
+{
+#if 0 //binary safe
+        int sl = strlen(str);
+        if (n >= sl)
+                n = sl;
+#endif
+        if (*s == NULL)
+                *s = heap_string_alloc(n);
+
+        for (size_t i = 0; i < n; ++i)
+        {
+                heap_string_push(s, *(char*)(str + i));
+        }
+}
+
+//binary unsafe, it won't change the existing data, but won't add any data past \0
+void heap_string_append(heap_string *s, const char *str)
+{
+        heap_string_appendn(s, str, strlen(str));
+}
+#endif
+#endif
---
+++ parse.h
@@ -0,0 +1,168 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+#include <stdio.h> //FILE
+#include <stdlib.h> //atof
+#include <ctype.h> //isspace
+//should probably make heap_string a struct and forward declare it in the func prototype below
+#include "heap_string.h"
+
+#ifndef PARSE_IMPL
+void parse_whitespace(FILE *fp);
+void parse_skip_line(FILE *fp);
+int parse_float(FILE *fp, float* out);
+int parse_float3(FILE *fp, float *v);
+/* don't forget to free ident! */
+int parse_ident(FILE *fp, heap_string *ident);
+int parse_character(FILE *fp, int ch);
+int parse_characters(FILE *fp, const char *str);
+int parse_ident_to_buffer(FILE *fp, char *buf, size_t bufsz, int *overflow);
+#else
+static int fpeekc(FILE *fp)
+{
+    int c = fgetc(fp);
+    ungetc(c, fp);
+    return c;
+}
+
+void parse_whitespace(FILE *fp)
+{
+	while(1)
+	{
+		if(fpeekc(fp) == EOF || !isspace(fpeekc(fp)))
+			return;
+		fgetc(fp);
+	}
+}
+
+void parse_skip_line(FILE *fp)
+{
+	int c;
+	do
+	{
+		c = fgetc(fp);
+	} while(c != EOF && c != '\n');
+}
+
+int parse_float(FILE *fp, float* out)
+{
+	int c;
+	char string[128]; //let's just allow up to 128..
+	size_t stringindex = 0;
+	do
+	{
+		if(stringindex >= sizeof(string))
+			return 1;
+		c = fgetc(fp);
+		string[stringindex++ % sizeof(string)] = c;
+	} while(c != EOF && ( c == 'e' || isdigit(c) || c == '-' || c == '.' ));
+	*out = (float)atof(string);
+	ungetc(c, fp); //we've parsed 1 too many
+	return c == EOF ? 1 : 0;
+}
+
+int parse_float3(FILE *fp, float *v)
+{
+	parse_whitespace(fp);
+	if(parse_float(fp, &v[0]))
+		return 1;
+	parse_whitespace(fp);
+	if(parse_float(fp, &v[1]))
+		return 1;
+	parse_whitespace(fp);
+	if(parse_float(fp, &v[2]))
+		return 1;
+	parse_whitespace(fp);
+	return 0;
+}
+
+int parse_ident_to_buffer(FILE *fp, char *buf, size_t bufsz, int *overflow)
+{
+	if(overflow)
+	*overflow = 0;
+	parse_whitespace(fp);
+	int c;
+	size_t index = 0;
+	
+	for(;;)
+	{
+		if(index + 1 >= bufsz)
+		{
+			if(overflow)
+			*overflow = 1;
+			break;
+		}
+		c = fgetc(fp);
+		if(c == EOF || isspace(c))
+		{
+			ungetc(c, fp); //unget space
+			break;
+		}
+		buf[index++] = c;
+	}
+	buf[index] = '\0';
+	return c == EOF ? 1 : 0;
+}
+
+/* don't forget to free ident! */
+
+int parse_ident(FILE *fp, heap_string *ident)
+{
+	parse_whitespace(fp);
+	int c;
+	for(;;)
+	{
+		c = fgetc(fp);
+		if(c == EOF || isspace(c))
+		{
+			ungetc(c, fp); //unget space
+			break;
+		}
+		heap_string_push(ident, c);
+	}
+	if(c == EOF)
+		heap_string_free(ident);
+	return c == EOF ? 1 : 0;
+}
+#if 0
+int parse_ident(FILE *fp, heap_string *ident)
+{
+	parse_whitespace(fp);
+	int c;
+	do
+	{
+		c = fgetc(fp);
+		heap_string_push(ident, c);
+	} while(c != EOF && !isspace(c));
+	ungetc(c, fp); //eh, ungetting space..
+	if(c == EOF) //free string..
+		heap_string_free(ident);
+	return c == EOF ? 1 : 0;
+}
+#endif
+
+int parse_character(FILE *fp, int ch)
+{
+	parse_whitespace(fp);
+	if(fpeekc(fp) != ch)
+	{
+		printf("expected %c got %c at %ld\n", ch, fpeekc(fp), ftell(fp));
+		return 1;
+	}
+	fgetc(fp);
+	parse_whitespace(fp);
+	return 0;
+}
+
+int parse_characters(FILE *fp, const char *str)
+{
+	size_t len = strlen(str);
+	for(size_t i = 0; i < len; ++i)
+	{
+		if(parse_character(fp, str[i]))
+			return 1;
+	}
+	return 0;
+}
+#endif
+#endif
