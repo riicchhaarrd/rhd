376be6b96d5b2f15275d95cd1b8dadc97a9c6a35
Richard <riicchhaarrd@users.noreply.github.com>
2022-03-13 17:33:13 +0100

    hash map with custom allocator

--- hash_map.h
+++ hash_map.h
@@ -42,11 +42,13 @@ struct hash_map
 	
 	deallocator_t on_key_removal_fn;
 	int distinct;
+	void *custom_allocator_userptr;
+	custom_allocator_fn_t custom_allocator_fn;
 };
 
 #ifndef HASH_MAP_IMPL
 //public API
-extern struct hash_map *hash_map_create_data(size_t data_size);
+extern struct hash_map *hash_map_create_data(size_t data_size, void *custom_allocator_userptr, custom_allocator_fn_t custom_allocator_fn);
 extern void hash_map_destroy(struct hash_map **hmp);
 extern void *hash_map_find(struct hash_map *ht, const char *key);
 extern int hash_map_insert_data(struct hash_map *ht, const char *key, unsigned char *data, size_t data_size);
@@ -75,9 +77,15 @@ void hash_map_set_on_key_removal(struct hash_map *hm, deallocator_t fn)
 	hm->on_key_removal_fn = fn;
 }
 
-HM_STATIC struct hash_bucket *hash_allocate_buckets(size_t num_buckets)
+HM_STATIC struct hash_bucket *hash_allocate_buckets(struct hash_map *hm, size_t num_buckets)
 {
-	struct hash_bucket *buckets = memory_allocate(sizeof(struct hash_bucket) * num_buckets);
+	struct hash_bucket *buckets = NULL;
+	
+	if(hm->custom_allocator_fn && hm->custom_allocator_userptr)
+		buckets = hm->custom_allocator_fn(hm->custom_allocator_userptr, sizeof(struct hash_bucket) * num_buckets);
+	else
+		buckets = memory_allocate(sizeof(struct hash_bucket) * num_buckets);
+	
 	for(size_t i = 0; i < num_buckets; ++i)
 	{
 		buckets[i].head = NULL; //all empty lists..
@@ -86,11 +94,17 @@ HM_STATIC struct hash_bucket *hash_allocate_buckets(size_t num_buckets)
 	return buckets;
 }
 
-struct hash_map *hash_map_create_data(size_t data_size)
+struct hash_map *hash_map_create_data(size_t data_size, void *custom_allocator_userptr, custom_allocator_fn_t custom_allocator_fn)
 {
-	struct hash_map *ht = memory_allocate(sizeof(struct hash_map));
+	struct hash_map *ht = NULL;
+	if(custom_allocator_fn && custom_allocator_userptr)
+		ht = custom_allocator_fn(custom_allocator_userptr, sizeof(struct hash_map));
+	else
+		ht = memory_allocate(sizeof(struct hash_map));
+	ht->custom_allocator_fn = custom_allocator_fn;
+	ht->custom_allocator_userptr = custom_allocator_userptr;
 	ht->num_entries = 0;
-	ht->buckets = hash_allocate_buckets(HASH_BUCKET_SIZE);
+	ht->buckets = hash_allocate_buckets(ht, HASH_BUCKET_SIZE);
 	ht->bucket_size = HASH_BUCKET_SIZE;
 	ht->data_size = data_size;
 	ht->distinct = 1;
@@ -195,22 +209,33 @@ void hash_map_dump(struct hash_map *hm)
 	#endif
 }
 
-HM_STATIC struct hash_bucket_entry *hash_bucket_entry_create(const char *key, unsigned char *data, size_t data_size)
+HM_STATIC struct hash_bucket_entry *hash_bucket_entry_create(struct hash_map *hm, const char *key, unsigned char *data, size_t data_size)
 {
 	unsigned long hashed_key = hash_string(key);
-	struct hash_bucket_entry *entry = memory_allocate(sizeof(struct hash_bucket_entry) + data_size);
+	struct hash_bucket_entry *entry = NULL;
+	
+	if(hm->custom_allocator_fn && hm->custom_allocator_userptr)
+		entry = hm->custom_allocator_fn(hm->custom_allocator_userptr, sizeof(struct hash_bucket_entry) + data_size);
+	else
+		entry = memory_allocate(sizeof(struct hash_bucket_entry) + data_size);
+	
 	entry->hash = hashed_key;
 	int kl = strlen(key);
-	entry->key = memory_allocate(kl + 1); //DON'T FORGET TO FREE THIS KEY
+	
+	if(hm->custom_allocator_fn && hm->custom_allocator_userptr)
+		entry = hm->custom_allocator_fn(hm->custom_allocator_userptr, kl + 1);
+	else
+		entry->key = memory_allocate(kl + 1); //DON'T FORGET TO FREE THIS KEY
+	
 	std_strncpy_s(entry->key, kl + 1, key, kl);
 	entry->next = NULL;
 	memcpy(entry->data, data, data_size);
 	return entry;
 }
 
-HM_STATIC void hash_bucket_insert(struct hash_bucket *bucket, const char *key, unsigned char *data, size_t data_size)
+HM_STATIC void hash_bucket_insert(struct hash_map *hm, struct hash_bucket *bucket, const char *key, unsigned char *data, size_t data_size)
 {
-	struct hash_bucket_entry *entry = hash_bucket_entry_create(key, data, data_size);
+	struct hash_bucket_entry *entry = hash_bucket_entry_create(hm, key, data, data_size);
 	
 	++bucket->size;
 	
@@ -229,7 +254,7 @@ HM_STATIC void hash_bucket_insert(struct hash_bucket *bucket, const char *key, u
 HM_STATIC void hash_map_rehash(struct hash_map *hm)
 {
 	size_t new_bucket_size = hm->bucket_size * 2;
-	struct hash_bucket *new_buckets = hash_allocate_buckets(new_bucket_size);
+	struct hash_bucket *new_buckets = hash_allocate_buckets(hm, new_bucket_size);
 	//fill the new buckets with old data
 	for(size_t i = 0; i < hm->bucket_size; ++i)
 	{
@@ -241,7 +266,7 @@ HM_STATIC void hash_map_rehash(struct hash_map *hm)
 		{
 			struct hash_bucket *new_bucket = &new_buckets[cur->hash % new_bucket_size];
 			//hash map num entries stays the same, we're just rehashing, not adding new entries
-			hash_bucket_insert(new_bucket, cur->key, cur->data, hm->data_size);
+			hash_bucket_insert(hm, new_bucket, cur->key, cur->data, hm->data_size);
 			cur = cur->next;
 		}
 		//free old bucket
@@ -265,7 +290,7 @@ int hash_map_insert_data(struct hash_map *ht, const char *key, unsigned char *da
 	
 	++ht->num_entries;
 	
-	hash_bucket_insert(bucket, key, data, data_size);
+	hash_bucket_insert(ht, bucket, key, data, data_size);
 	
 	//probably should rehash before insertion
 	if(ht->num_entries >= HASH_LOAD_FACTOR * ht->bucket_size)
@@ -281,7 +306,9 @@ int hash_map_insert_data(struct hash_map *ht, const char *key, unsigned char *da
 #endif
 
 #define hash_map_create(type) \
-	hash_map_create_data(sizeof(type))
+	hash_map_create_data(sizeof(type), NULL, NULL)
+#define hash_map_create_with_custom_allocator(type, userptr, allocator_fn) \
+	hash_map_create_data(sizeof(type), userptr, allocator_fn)
 
 #define hash_map_insert(ht, key, value) \
 	hash_map_insert_data(ht, key, (unsigned char*)&(value), sizeof(value))
